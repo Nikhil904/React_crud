8502e94e86c3c1248890f78cb56ea3fda1acebfc
Nikhil Jain <jainnikhil472@gmail.com>
2022-02-21 15:53:41 +0530

    Complete Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Pages/Home";
+import Contact from "./Components/Pages/Contact";
+import About from "./Components/Pages/About";
+import Navbar from "./Components/Layout/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Error from "./Components/Pages/Error";
 function App() {
   return (
-    <>
- <Home />
-      
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="*" element={<Error />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
---
+++ src/Components/Pages/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+const About = () => {
+    return (
+      <>
+      <div className="container">
+        <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+        <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+        <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+        <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+      </div>
+        
+      </>
+    );
+  }
+  
+  export default About;
